5b83b9f28c64fa180b1660e687068a9edf5bee45
Rheina <rheina.tamara@outlook.com>
2020-11-30 16:59:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   Object.assign(playlist, {[artistName]: songTitle});
 }
 console.log (updatePlaylist(playlist, `Phil Ochs`, `Smile`));
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName;
+}
+console.log(removeFromPlaylist(playlist,'tameImpala'))
