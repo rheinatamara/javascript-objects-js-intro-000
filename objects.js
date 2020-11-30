4b5ba600ab11e1e05f001fb36acca516d0e250d5
Rheina <rheina.tamara@outlook.com>
2020-11-30 16:49:30 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
   tameImpala : 'The Less I Know The Better'
 }
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign(playlist, {artistName: 'songTitle'});
+  Object.assign(playlist, {artistName: songTitle});
 }
-console.log (updatePlaylist(playlist, lilyAllen, 'Smile'));
+console.log (updatePlaylist(playlist, 'lilyAllen', 'Smile'));
